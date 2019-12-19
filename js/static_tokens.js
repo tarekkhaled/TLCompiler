b72178338bcfdc28db3a976562e8639a85affd55
tarek tito <tarekt820@gmail.com>
2019-12-19 03:43:52 +0200

    static tokens for tiny language program

---
+++ js/static_tokens.js
@@ -0,0 +1,133 @@
+const tokens = [
+    {
+        tokenType : 'READ',
+        tokenValue : 'read'
+    },
+    {
+        tokenType : 'IDENTIFIER',
+        tokenValue : 'x'
+    },
+    {
+        tokenType : 'SEMI',
+        tokenValue : ';'
+    },
+    {
+        tokenType : 'IF',
+        tokenValue : 'if'
+    },
+    {
+        tokenType : 'NUMBER',
+        tokenValue : '0'
+    },
+    {
+        tokenType : 'LESSTHAN',
+        tokenValue : '<'
+    },
+    {
+        tokenType : 'IDENTIFIER',
+        tokenValue : 'x'
+    },
+    {
+        tokenType : 'THEN',
+        tokenValue : 'then'
+    },
+    {
+        tokenType : 'IDENTIFIER',
+        tokenValue : 'fact'
+    },
+    {
+        tokenType : 'ASSIGN',
+        tokenValue : ':='
+    },
+    {
+        tokenType : 'NUMBER',
+        tokenValue : '1'
+    },
+    {
+        tokenType : 'SEMI',
+        tokenValue : ';'
+    },
+    {
+        tokenType : 'REPEAT',
+        tokenValue : 'repeat'
+    },
+    {
+        tokenType : 'IDENTIFIER',
+        tokenValue : 'fact'
+    },
+    {
+        tokenType : 'ASSIGN',
+        tokenValue : ':='
+    },
+    {
+        tokenType : 'IDENTIFIER',
+        tokenValue : 'fact'
+    },
+    {
+        tokenType : 'MULT',
+        tokenValue : '*'
+    },
+    {
+        tokenType : 'IDENTIFIER',
+        tokenValue : 'x'
+    },
+    {
+        tokenType : 'SEMI',
+        tokenValue : ';'
+    },
+    {
+        tokenType : 'IDENTIFIER',
+        tokenValue : 'x'
+    },
+    {
+        tokenType : 'ASSIGN',
+        tokenValue : ':='
+    },
+    {
+        tokenType : 'IDENTIFIER',
+        tokenValue : 'x'
+    },
+    {
+        tokenType : 'MINUS',
+        tokenValue : '-'
+    },
+    {
+        tokenType : 'NUMBER',
+        tokenValue : '1'
+    },
+    {
+        tokenType : 'UNTIL',
+        tokenValue : 'until'
+    },
+    {
+        tokenType : 'IDENTIFIER',
+        tokenValue : 'x'
+    },
+    {
+        tokenType : 'EQUAL',
+        tokenValue : '='
+    },
+    {
+        tokenType : 'NUMBER',
+        tokenValue : '0'
+    },
+    {
+        tokenType : 'SEMI',
+        tokenValue : ';'
+    },
+    {
+        tokenType : 'WRITE',
+        tokenValue : 'write'
+    },
+    {
+        tokenType : 'IDENTIFIER',
+        tokenValue : 'fact'
+    },
+    {
+        tokenType : 'END',
+        tokenValue : 'end'
+    },
+]
+
+
+module.exports = tokens;
